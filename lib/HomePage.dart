67b2e5bbba56dfc958cc7d284d5968b40e8dc2f9
joaomalfatti <joao.cursos1210@gmail.com>
2024-01-31 17:39:51 -0400

    Foi finalizado a parte da construção da interface

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -8,8 +8,51 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  String _preco = "0";
+
+  void _recuperarPreco(){}
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset("imagens/bitcoin.png"),
+              Padding(
+                  padding: EdgeInsets.only(top: 30, bottom: 30),
+                  child: Text(
+                      "R\$ " + _preco,
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.orange,
+                    padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
+                  ),
+                  onPressed: (){
+                    _recuperarPreco();
+                  },
+                  child: Text(
+                    "Atualizar",
+                      style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,5 +5,5 @@ void main (){
   runApp(MaterialApp(
     home: HomePage(),
     debugShowCheckedModeBanner: false,
-  ))
+  ));
 }
