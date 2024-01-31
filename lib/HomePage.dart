33356d070f917ff45d05776a011ab731005a687e
joaomalfatti <joao.cursos1210@gmail.com>
2024-01-31 18:15:51 -0400

    Foi feito a lógica do aplicativo, onde o mesmo consome numa API

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,17 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   String _preco = "0";
 
-  void _recuperarPreco(){}
+  void _recuperarPreco() async {
+    String url = "https://blockchain.info/ticker";
+
+    http.Response response = await http.get(Uri.parse(url));
+
+    Map<String, dynamic> retorno = json.decode(response.body);
+
+    setState(() {
+      _preco = retorno["BRL"]["buy"].toString();
+    });
+  }
+
+  void _limparResultado() {
+    setState(() {
+      _preco = "0";
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,31 +42,53 @@ class _HomePageState extends State<HomePage> {
             children: <Widget>[
               Image.asset("imagens/bitcoin.png"),
               Padding(
-                  padding: EdgeInsets.only(top: 30, bottom: 30),
-                  child: Text(
-                      "R\$ " + _preco,
-                    style: TextStyle(
-                      fontSize: 35,
-                      fontWeight: FontWeight.bold
-                    ),
+                padding: EdgeInsets.only(top: 30, bottom: 30),
+                child: Text(
+                  "R\$ " + _preco,
+                  style: const TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
                   ),
+                ),
               ),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.orange,
-                    padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
-                  ),
-                  onPressed: (){
-                    _recuperarPreco();
-                  },
-                  child: Text(
-                    "Atualizar",
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange,
+                      padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
+                    ),
+                    onPressed: () {
+                      _recuperarPreco();
+                    },
+                    child: const Text(
+                      "Atualizar",
                       style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white,
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-              ),
+                  SizedBox(width: 20),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
+                    ),
+                    onPressed: () {
+                      _limparResultado();
+                    },
+                    child: const Text(
+                      "Limpar",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
