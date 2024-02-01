a28417d20409b7ceadfd57f8a359a109a5728a7f
joaomalfatti <joao.cursos1210@gmail.com>
2024-02-01 14:14:19 -0400

    Falta colocar url para consumir a API

--- lib/EthereumPage.dart
+++ lib/EthereumPage.dart
@@ -1,15 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 class EthereumPage extends StatefulWidget {
-  const EthereumPage({super.key});
+  const EthereumPage({Key? key});
 
   @override
   State<EthereumPage> createState() => _EthereumPageState();
 }
 
 class _EthereumPageState extends State<EthereumPage> {
+  String _preco = "0";
+
+  void _recuperarPreco() async {
+    String url = "https://blockchain.info/ticker";
+
+    http.Response response = await http.get(Uri.parse(url));
+
+    Map<String, dynamic> retorno = json.decode(response.body);
+
+    setState(() {
+      _preco = retorno["BRL"]["buy"].toString();
+    });
+  }
+
+  void _limparResultado() {
+    setState(() {
+      _preco = "0";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset("imagens/Ethereum.png"),
+              Padding(
+                padding: EdgeInsets.only(top: 30, bottom: 30),
+                child: Text(
+                  "R\$ " + _preco,
+                  style: const TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange,
+                      padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
+                    ),
+                    onPressed: () {
+                      _recuperarPreco();
+                    },
+                    child: const Text(
+                      "Atualizar",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
+                    ),
+                    onPressed: () {
+                      _limparResultado();
+                    },
+                    child: const Text(
+                      "Limpar",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
